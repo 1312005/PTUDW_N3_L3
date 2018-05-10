b59643f05205bb45d7e9a43e5b62cbe61507c524
dinhduy <duytrandinh95@gmail.com>
2018-05-10 18:55:27 +0700

    no message

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-var express = require("express");
-var app     = express();
-var path    = require("path");
-app.use(express.static(__dirname + '/public'));
+var express = require('express');
+var exphbs = require('express-handlebars');
+var path = require('path');
 
+var app = express();
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/index.html'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: 'views/_layouts/'
+}));
+app.set('view engine', 'handlebars');
 
-  //__dirname : It will resolve to your project folder.
+app.use(express.static(
+	path.resolve(__dirname, 'public')
+));
+
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
-
-app.listen(3000);
-
-console.log("Running at Port 3000");
+app.listen(3000, () => {
+    console.log('server running on port 3000');
+});
